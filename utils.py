22e0310681891290d87330a3ffe0840af8dcb684
Vivek Shah <Vivek@MBP1612.local>
2020-08-15 15:49:28 +0530

    Working towards changing illuminance for face augmentation

--- utils.py
+++ utils.py
@@ -4,6 +4,11 @@ class ImageAugmentation:
     def _reflect(self, image, path):
         flipHorizontal = cv2.flip(image, 1)
         cv2.imwrite(path[:-4] + '-augmented-r.png', flipHorizontal)
+    def adjust_gamma(image, gamma=1.0):
+        invGamma = 1.0 / gamma
+        table = np.array([((i / 255.0) ** invGamma) * 255 
+        for i in np.arange(0, 256)]).astype("uint8")
+        return cv2.LUT(image, table)
     def execute(self):
         for person in os.listdir('dataset/'):
             personPath = 'dataset/' + person + '/'
