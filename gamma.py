ee012859e43184e38e40bfcb624b3b526c10d2f2
Vivek Shah <Vivek@MBP1612.local>
2020-08-15 15:22:37 +0530

    Adding branch for working with lighting

---
+++ gamma.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+def adjust_gamma(image, gamma=1.0):
+
+   invGamma = 1.0 / gamma
+   table = np.array([((i / 255.0) ** invGamma) * 255
+      for i in np.arange(0, 256)]).astype("uint8")
+
+   return cv2.LUT(image, table)
+
+x = 'C:/Users/524316/Desktop/stack/test.jpg'  #location of the image
+original = cv2.imread(x, 1)
+cv2.imshow('original',original)
+
+gamma = 0.5                                   # change the value here to get different result
+adjusted = adjust_gamma(original, gamma=gamma)
+cv2.putText(adjusted, "g={}".format(gamma), (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+cv2.imshow("gammam image 1", adjusted)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
