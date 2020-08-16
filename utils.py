33b90eecbfc5f1ead08b9b60619f38705fd2e850
Vivek Shah <Vivek@MBP1612-2.local>
2020-08-16 15:03:08 +0530

    Adding reflecting and changing lighting support

--- utils.py
+++ utils.py
@@ -4,7 +4,9 @@ from tqdm import tqdm
 class ImageAugmentation:
     def _reflect(self, image, path):
         flipHorizontal = cv2.flip(image, 1)
-        cv2.imwrite(path[:-4] + '-augmentReflect.png', flipHorizontal)
+        outPath = path[:-4] + '-reflected.png'
+        cv2.imwrite(outPath, flipHorizontal)
+        return outPath
     def _adjust_gamma(self, image, gamma=1.0):
         invGamma = 1.0 / gamma
         table = np.array([((i / 255.0) ** invGamma) * 255 
@@ -14,12 +16,7 @@ class ImageAugmentation:
         gammas = [0.5, 0.7, 1.3, 1.5]
         for gamma in gammas:
             adjusted = self._adjust_gamma(image, gamma=gamma)
-            cv2.imwrite(path[:-4]+'-augmentLight-' + str(gamma) + '.png', adjusted)
-    def _rotate(self, image, path):
-        angles = [-75, -60, -45, -30, -15, 15, 30, 45, 60, 75]
-        for angle in angles:
-            rotatedImage = imutils.rotate(image, angle)
-            cv2.imwrite(path[:-4]+'-augmentRotate-'+str(angle) + '.png', rotatedImage)
+            cv2.imwrite(path[:-4]+'-augmented-' + str(gamma) + '.png', adjusted)
     def execute(self):
         people = os.listdir('dataset/')
         people.remove('unknown')
@@ -27,10 +24,11 @@ class ImageAugmentation:
             personPath = 'dataset/' + person + '/'
             print(person)
             for image in tqdm(os.listdir(personPath)):
-                if 'augment' not in image and '.DS_Store' not in image:
+                if 'augmented' not in image and '.DS_Store' not in image:
                     path = personPath + image
                     photo = cv2.imread(path)
-                    self._reflect(photo, path)
+                    reflectedPath = self._reflect(photo, path)
+                    reflectedPhoto = cv2.imread(reflectedPath)
                     self._changeLighting(photo, path)
-                    self._rotate(photo, path)
+                    self._changeLighting(reflectedPhoto, reflectedPath)
 ImageAugmentation().execute()
